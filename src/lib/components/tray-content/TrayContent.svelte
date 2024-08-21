2fd777e18cf7fd8559f4238fdabf50ab4a6cd142
zfleury <zach@fleury.lol>
2024-08-21 11:45:18 -0500

    shown dressings

--- src/lib/components/tray-content/TrayContent.svelte
+++ src/lib/components/tray-content/TrayContent.svelte
@@ -29,10 +29,25 @@
 	</div>
 </div>
 <div>
-	<h2>Dressings</h2>
-	<div class="flex flex-col">
-		<div class="flex items-center border">
-			<span>Avocado Lime Ranch: {processedOrder.avocadoLimeRanch}</span>
-		</div>
+	<h2>Dressings:</h2>
+	<div class="flex flex-col border">
+		<span class="dressing">Avocado Lime Ranch: {processedOrder.dressings.avoRanch}</span>
+		<span class="dressing">Garden Herb Ranch: {processedOrder.dressings.ranch}</span>
+		<span class="dressing">Zesty Apple Cider: {processedOrder.dressings.zestyAppleCider}</span>
+		<span class="dressing"
+			>Fat Free Honey Mustard: {processedOrder.dressings.fatFreeHoneyMustard}</span
+		>
+		<span class="dressing">Light Balsamic: {processedOrder.dressings.lightBalsamicVinaigrette}</span
+		>
+		<span class="dressing">Light Italian: {processedOrder.dressings.lightItalian}</span>
+		<span class="dressing">Creamy Salsa: {processedOrder.dressings.creamySalsa}</span>
 	</div>
 </div>
+
+<style>
+	.dressing {
+		padding: 0.5rem;
+		display: flex;
+		flex-direction: column;
+	}
+</style>
